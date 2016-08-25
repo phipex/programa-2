d6208daa03f02bf76e2604782666f45d62f5a247
phipex <phipex@yahoo>
2016-08-25 13:12:27 -0500

    modificado las clases para realizar la regresion

--- gulpfile.js
+++ gulpfile.js
@@ -77,5 +77,5 @@ gulp.task('clean', function () {
 });
 
 gulp.task('prepublish', ['nsp', 'babel']);
-gulp.task('default', ['static', 'test', 'coveralls']);
-//gulp.task('default', ['test', 'coveralls']);
+// gulp.task('default', ['static', 'test', 'coveralls']);
+gulp.task('default', ['test', 'coveralls']);
